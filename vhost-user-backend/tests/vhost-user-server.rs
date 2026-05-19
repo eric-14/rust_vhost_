894cb4acffccddfeeea34b90bbce18661cc6f2e8
EricMwangi <ericmwas01@gmail.com>
2026-05-19 18:27:28 +0300

    vhost-user-backend: replace eventSet from epoll to mio
    	Handle event breaks when using mio since it depends on epoll.
    	Fix: update handle_event to use EventSet
    
    Signed-off-by: EricMwangi <ericmwas01@gmail.com>

--- vhost-user-backend/tests/vhost-user-server.rs
+++ vhost-user-backend/tests/vhost-user-server.rs
@@ -117,7 +117,7 @@ impl VhostUserBackendMut for MockVhostBackend {
     fn handle_event(
         &mut self,
         _device_event: u16,
-        _evset: Interest,
+        _evset: EventSet,
         _vrings: &[VringRwLock],
         _thread_id: usize,
     ) -> Result<()> {
